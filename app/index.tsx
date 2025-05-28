88e6962a49de6d25e73222e3607c99e7340ddc4b
Agustin Negreira <anegreira2004@gmail.com>
2025-05-28 19:08:31 -0300

    Margintop main container

--- app/index.tsx
+++ app/index.tsx
@@ -11,8 +11,7 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#25292e',
+    marginTop: 140,
     alignItems: 'center',
     justifyContent: 'center',
   },
